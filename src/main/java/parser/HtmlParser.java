26297475351ab9df620011e2333e311e2a0a83f9
nedelweiss <edelweissnull@gmail.com>
2023-12-02 19:22:34 +0200

    added TODOs

--- src/main/java/parser/HtmlParser.java
+++ src/main/java/parser/HtmlParser.java
@@ -13,6 +13,7 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.regex.Pattern;
 
+// TODO: add threads
 public class HtmlParser {
 
     private static final Path INVALID_LINKS = Path.of(System.getProperty("user.dir") + "/" + "invalid_links.txt");
@@ -24,6 +25,7 @@ public class HtmlParser {
 
     public HtmlParser() {
         this.urlValidator = new UrlValidator();
+        // TODO: get rid of logic with files, make clean up
         try {
             if (!Files.exists(INVALID_LINKS)) {
                 Files.createFile(INVALID_LINKS);
@@ -36,26 +38,32 @@ public class HtmlParser {
         }
     }
 
-    // TODO: add logging and log the parsing process
+    // TODO: add logging (search some libraries) and log the parsing process
     public Set<String> parse(Set<String> currentLinks, int searchDeep, Set<String> resultedLinks) {
         try {
             if (searchDeep == 0) {
                 return resultedLinks;
             }
 
+            // TODO: search lib for time counting
+//            startTime
             Set<String> parsedLinks = new HashSet<>();
             for (String link : currentLinks) {
                 Document document = Jsoup.connect(link).get();
-                Elements elements = document.select("a");
+                Elements elements = document.select("a"); // TODO: add "a" to constant
 
                 parsedLinks.addAll(parseLinks(elements));
             }
+//            endTime
 
             resultedLinks.addAll(currentLinks);
             resultedLinks.addAll(parsedLinks);
 
+            // TODO:  logger.info(searchDeep, endTime - startTime)
+
             return parse(parsedLinks, searchDeep - RECURSION_STEP, resultedLinks);
         } catch (Exception e) {
+            // TODO: add logger for link & searchDeep
             throw new URLRequestException("Incorrect URL request, or the connection time is out", e);
         }
     }
@@ -64,8 +72,17 @@ public class HtmlParser {
     private Set<String> parseLinks(Elements elements) {
         Set<String> links = new HashSet<>();
         for (Element element : elements) {
-            String link = element.attr("abs:href");
+            String link = element.attr("abs:href"); // TODO: add "abs:href" to constant
+            System.out.println(link);
+
+            // TODO: get rid of links with internalization
+            // https://stackoverflow.com/questions/7223788/are-there-constants-for-language-codes-in-java-or-in-a-java-library
+//            Set<String> isoLanguages = Set.of(Locale.getISOLanguages());
             if (hasValidProtocol(link) && !hasQueryParameters(link) && !isMedia(link)) {
+                // TODO: implement methods
+//            String str1 = deleteSharp(link);
+//            String str2 = deleteSlash(link);
+
                 links.add(link);
             }
         }
